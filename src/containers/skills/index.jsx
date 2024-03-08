457562f17a2fd9abb1da4f084cc6634e0f445f00
Julien Mattei <j.mattei@me.com>
2024-03-08 11:44:44 +0100

    ajout des logo dans skills

--- src/containers/skills/index.jsx
+++ src/containers/skills/index.jsx
@@ -1,19 +1,35 @@
 import './style.css'
+import data from "../../datas/skills.json"
+
 
 function Skills () {
+    const frontEnd = data.filter((skillFront) => skillFront.Type === "FrontEnd")
+    console.log(frontEnd)
+    const backEnd = data.filter((skillBack) => skillBack.Type === "BackEnd")
     return <div className='skills'>
         <h2 id="skills">My Skills</h2>
         <div className='skills-list'>
             <div className='skills-list-front'>
                 <h3>FrontEnd</h3>
                 <div className='skills-front'>
-                    <img src="" alt="" />
+                    {frontEnd.map((front, idx) => 
+                        <div key={idx} className='skills-content'>
+                            <img  src={front.logo} alt={front.langage} className='skills-logo'/>
+                        </div>
+                        
+                    )}
+                    
                 </div>
             </div>
             <div className='skills-list-back'>
                 <h3>BackEnd</h3>
                 <div className='skills-back'>
-                    <img src="" alt="" />
+                {backEnd.map((back, idx) => 
+                        <div key={idx} className='skills-content'>
+                            <img  src={back.logo} alt={back.langage} className='skills-logo'/>
+                        </div>
+                        
+                    )}
                 </div>
             </div>
             
